dc20c0e11bcc1676944f62496d75a4bbfc97b3ee
Mukhammadyusuf Abdurakhimov <mukhammadyusuf_abdurakhimov@epam.com>
2022-05-04 11:07:03 +0500

    Navbar

--- components/HeaderItem.js
+++ components/HeaderItem.js
@@ -1,14 +1,17 @@
+import Link from 'next/link';
+
 const HeaderItem = ({ title, Icon }) => {
   return (
-    <div className="flex flex-col items-center cursor-pointer w-12 sm:w-20 hover:text-white group">
-      <Icon className="h-8 mb-1 group-hover:animate-bounce" />
-      <a
-        className="tracking-widest opacity-0 group-hover:opacity-100"
-        href={`/${title}`}
-      >
-        {title}
+    <Link href={`/${title}`}>
+      <a>
+        <div className="flex flex-col items-center cursor-pointer w-12 sm:w-20 hover:text-white group">
+          <Icon className="h-8 mb-1 group-hover:animate-bounce" />
+          <p className="tracking-widest opacity-0 group-hover:opacity-100">
+            {title}
+          </p>
+        </div>
       </a>
-    </div>
+    </Link>
   );
 };
 
