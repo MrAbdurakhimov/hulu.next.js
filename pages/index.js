8a70f7c485f8765c78c0666ae2af76367fe23807
Mukhammadyusuf Abdurakhimov <mukhammadyusuf_abdurakhimov@epam.com>
2022-05-04 11:26:19 +0500

    delete api

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Header from '../components/Header';
 import Nav from '../components/Nav';
+import Results from '../components/Results';
 
 export default function Home() {
   return (
@@ -8,11 +9,9 @@ export default function Home() {
       <Head>
         <title>Home | Hulu 2.0</title>
       </Head>
-      {/* Header */}
       <Header />
-      {/* Navbar */}
       <Nav />
-      {/* Results */}
+      <Results />
     </div>
   );
 }
